996dad3866d26de0b807945d6ec83d327a29f3ab
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-06-03 12:38:56 -0400

    updated select form and added bought and sold feature/tg

--- src/components/AddAsset.js
+++ src/components/AddAsset.js
@@ -1,25 +1,27 @@
 
 const newAsset = (props) => {
     return (
-        <div className="submit-form-container">
+        <div className="asset-container">
           <h3>Submit a new NFT</h3>
         <form onSubmit={props.submitAsset}>
+          Bought:  <input type="checkbox" onChange={props.handleBought}/><br/>
+          <br/>
           Image: <input type="url" onChange={props.handlePrimary_Image}/><br/>
           <br/>
           Title:  <input type="text" onChange={props.handleTitle}/><br/>
           <br/>
           Description: <input type="text" onChange={props.handleDescription}/><br/>
           <br/>
-          Date created: <input type="text" onChange={props.handleCreatedDate}/><br/>
-          <br/>
+          {/* Date created: <input type="text" onChange={props.handleCreatedDate}/><br/>
+          <br/> */}
           {/* Additonal images: <input type="text" onChange ={props.handleAdditionalImages}/><br/>
           <br/> */}
           tags:  <input type="text" onChange ={props.handleTags}/><br/>
           <br/>
           Artist:  <input type="text" onChange ={props.handleArtist}/><br/>
           <br/>
-          Owner:  <input type="text" onChange ={props.handleOwner}/><br/>
-          <br/>
+          {/* Owner:  <input type="text" onChange ={props.handleOwner}/><br/>
+          <br/> */}
           {/* Title of collection:  <input type="text" onChange ={props.handleCollection_Title}/><br/>
           <br/> */}
           {/* Collection image:  <input type="text" onChange ={props.handleCollection_Image}/><br/>
@@ -28,11 +30,11 @@ const newAsset = (props) => {
           <br/>
           # of Bids:  <input type="number" onChange ={props.handleBid_Count}/><br/>
           <br/>
-          Bid price:  <input type="number" onChange ={props.handleBid_Price}/><br/>
+          Bid price:  <input type="text" onChange ={props.handleBid_Price}/><br/>
           <br/>
           # of sales:  <input type="number" onChange ={props.handleSales_Count}/><br/>
           <br/>
-          Sale price:  <input type="number" onChange ={props.handleSales_Price}/><br/>
+          Sale price:  <input type="text" onChange ={props.handleSales_Price}/><br/>
           <br/>
           <input type="submit" value="Add NFT"/>
         </form>
--- src/components/EditNFT.js
+++ src/components/EditNFT.js
@@ -2,37 +2,39 @@ const EditNFT = (props) => {
     return (
       <div>
         <form onSubmit={(event)=>{props.handleEdit(event, props.assets)}}>
-         Image: <input type="text" onChange={props.handlePrimary_Image} placeholder={props.assets.primary_Image}/><br/>
-        <br/> 
+        Bought:  <input type="checkbox" onChange={props.handleBought} placeholder={props.assets.bought}/><br/>
+          <br/>
+         {/* Image: <input type="text" onChange={props.handlePrimary_Image} placeholder={props.assets.primary_Image}/><br/>
+        <br/>  */}
         Title:  <input type="text" onChange={props.handleTitle} placeholder={props.assets.title}/><br/>
         <br/>
-         Description: <input type="text" onChange={props.handleDescription}/><br/>
+         Description: <input type="text" onChange={props.handleDescription} placeholder={props.assets.description}/><br/>
         <br/>
-        Date created: <input type="text" onChange={props.handleCreatedDate}/><br/>
-        <br/>
+        {/* Date created: <input type="text" onChange={props.handleCreatedDate}/><br/>
+        <br/> */}
         {/* Additonal images: <input type="text" onChange ={props.handleAdditionalImages}/><br/>
         <br/> */}
-        tags:  <input type="text" onChange ={props.handleTags}/><br/>
+        Tags:  <input type="text" onChange ={props.handleTags} placeholder={props.assets.tags}/><br/>
         <br/>
-        Artist:  <input type="text" onChange ={props.handleArtist}/><br/>
+        Artist:  <input type="text" onChange ={props.handleArtist} placeholder={props.assets.artist}/><br/>
         <br/>
-        Owner:  <input type="text" onChange ={props.handleOwner}/><br/>
-        <br/>
+        {/* Owner:  <input type="text" onChange ={props.handleOwner}/><br/>
+        <br/> */}
         {/* Title of collection:  <input type="text" onChange ={props.handleCollection_Title}/><br/>
         <br/> */}
         {/* Collection image:  <input type="text" onChange ={props.handleCollection_Image}/><br/>
         <br/> */}
-        Data url:  <input type="text" onChange ={props.handleData_URL}/><br/>
+        Data url:  <input type="text" onChange ={props.handleData_URL} placeholder={props.assets.data_URL}/><br/>
         <br/>
-        # of Bids:  <input type="number" onChange ={props.handleBid_Count}/><br/>
+        # of Bids:  <input type="number" onChange ={props.handleBid_Count} placeholder={props.assets.bid_count}/><br/>
         <br/>
-        Bid price:  <input type="number" onChange ={props.handleBid_Price}/><br/>
+        Bid price:  <input type="text" onChange ={props.handleBid_Price} placeholder={props.assets.bid_price}/><br/>
         <br/>
-        # of sales:  <input type="number" onChange ={props.handleSales_Count}/><br/>
+        # of sales:  <input type="number" onChange ={props.handleSales_Count} placeholder={props.assets.sales_count}/><br/>
         <br/>
-        Sale price:  <input type="number" onChange ={props.handleSales_Price}/><br/>
+        Sale price:  <input type="text" onChange ={props.handleSales_Price} placeholder={props.assets.sales_price}/><br/>
         <br/> 
-        <input type="submit" value="Submit edit"/>
+        <input type="submit" value="Submit"/>
       </form>
       {/* <button onClick={props.editView}> Cancel </button> */}
       </div>
